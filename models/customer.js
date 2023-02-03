0930f6b52102365aebdcc0055fc9c7e571a5136f
Himanshu0422 <himanshumittal035@gmail.com>
2023-02-03 13:51:01 +0530

    customer model

---
+++ models/customer.js
@@ -0,0 +1,29 @@
+const Joi = require('Joi');
+const mongoose = require('mongoose');
+
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    isGold: {
+        type: Boolean,
+        default: false
+    },
+    phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+}));
+
+const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required()
+});
+
+exports.Customer = Customer;
+exports.schema = schema;
