44b8cac8370ab130f1851c6384285bfd2e0ccaef
Himanshu0422 <himanshumittal035@gmail.com>
2023-02-04 14:26:12 +0530

    added models

---
+++ models/genre.js
@@ -0,0 +1,21 @@
+const Joi = require('Joi');
+const mongoose = require('mongoose');
+
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
+
+const schema = Joi.object({
+    name: Joi.string().min(3).required()
+});
+
+exports.genreSchema = genreSchema;
+exports.Genre = Genre;
+exports.schema = schema;
---
+++ models/user.js
@@ -0,0 +1,45 @@
+const Joi = require('Joi');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    isAdmin:{
+        type: Boolean
+    }
+});
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+    return token;
+}
+
+const User = mongoose.model('User', userSchema);
+
+const schema = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+});
+
+exports.User = User;
+exports.schema = schema;
