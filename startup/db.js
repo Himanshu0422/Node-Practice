d23252a1e6061c00865eea4a418feeb237fbe2f5
Himanshu0422 <himanshumittal035@gmail.com>
2023-02-08 14:39:33 +0530

    db startup file

---
+++ startup/db.js
@@ -0,0 +1,8 @@
+const winston = require('winston');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+module.exports = function () {
+    mongoose.connect('mongodb://127.0.0.1:27017/vidly')
+        .then(() => winston.info('Connected'))
+}
