befc240c9d17277b44acb3bba604de62ac9cf095
Himanshu0422 <himanshumittal035@gmail.com>
2023-02-10 23:40:12 +0530

    logging file

---
+++ startup/logging.js
@@ -0,0 +1,18 @@
+const winston = require('winston');
+// require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function () {
+    winston.exceptions.handle(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+
+    process.on('unhandledRejection', (ex) => {
+        throw ex;
+    });
+
+
+    winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+    // winston.add(new winston.transports.MongoDB, {
+    //     db: 'mongodb://127.0.0.1:27017/vidly',
+    //     level: 'info'
+    // });
+}
