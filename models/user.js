e970dc3108bee78c81ae9f7148ec9eee008c1b9e
Himanshu0422 <himanshumittal035@gmail.com>
2023-04-21 18:36:28 +0530

    bug fixes

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const Joi = require('Joi');
+const Joi = require('joi');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,6 @@ mongoose.set('strictQuery', true);
 
 module.exports = function () {
     mongoose.connect('mongodb://127.0.0.1:27017/vidly')
-        .then(() => winston.info('Connected'))
+        .then(() => winston.info('Connected to mongo'))
+        .catch((err) => winston.info('Error'))
 }
